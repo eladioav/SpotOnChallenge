2fad7af49e56ed446037e19a6d3da1a13ae15d34
eladioav <eladioav@hotmail.com>
2019-05-02 12:49:26 -0500

    Initial Commit

---
+++ SpotOnChallenge/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  SpotOnChallenge
+//
+//  Created by Eladio Alvarez Valle on 5/2/19.
+//  Copyright © 2019 Eladio Alvarez Valle. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
